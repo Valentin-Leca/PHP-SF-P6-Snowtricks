5870f38e1f62414e9aeac4968fa648709c37ca99
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2022-06-03 12:09:45 +0200

    Add token link in email & change some entity asserts + UniqueEntity.

---
+++ src/Service/RegisterMail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+
+class RegisterMail {
+
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer) {
+
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function sendRegisterMail($user): void {
+
+        $email = (new TemplatedEmail())
+            ->from('testmailsymfonymailer@gmail.com')
+            ->to($user->getMail())
+            ->subject('Bienvenue sur Snowtricks !')
+            ->text('Sending emails is fun again!')
+            ->htmlTemplate('emails/createAccount.html.twig')
+            ->context(['mailAdress' => $user->getMail(),
+                'login' => $user->getLogin(),
+                'token' => $user->getToken(),
+            ]);
+
+        $this->mailer->send($email);
+    }
+
+}
