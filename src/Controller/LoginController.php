52d828c7355cc1b5127781a5bb7939451a96383a
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2022-06-03 17:23:08 +0200

    Add login template + LoginType form

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
-{
+class LoginController extends AbstractController {
+
     #[Route('/login', name: 'login', methods: ["POST", "GET"])]
-    public function index(): Response
-    {
+    public function login(): Response {
+
+        $user = new User();
+
+        $form = $this->createForm(LoginType::class, $user);
+
         return $this->render('login.html.twig', [
-            'controller_name' => 'LoginController',
+            'form' => $form->createView()
         ]);
     }
 }
