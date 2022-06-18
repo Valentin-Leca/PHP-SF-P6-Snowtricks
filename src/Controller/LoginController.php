83e734912194b34540188389503ffe41ba639834
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2022-06-18 11:30:27 +0200

    WIP password reset functionnality

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\ForgotPasswordType;
 use App\Service\ForgotPasswordMail;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
@@ -11,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -43,11 +43,11 @@ class LoginController extends AbstractController {
 
         $request = Request::createFromGlobals();
 
-        $userLogin = $request->request->get("login");
+        $userMail = $request->request->get("mail");
 
         $userRepository = $entityManager->getRepository(User::class);
 
-        $user = $userRepository->findOneBy(['login' => $userLogin]);
+        $user = $userRepository->findOneBy(['mail' => $userMail]);
 
         if ($user) {
 
@@ -57,14 +57,36 @@ class LoginController extends AbstractController {
 
             $this->addFlash(type: "success", message: "Demande de réinitialisation de mot de passe envoyé !");
         } elseif($user != true) {
-            $this->addFlash(type: "error", message: "Ce login n'est associé à aucun compte utilisateur !");
+            $this->addFlash(type: "error", message: "Cette adresse e-mail n'est associé à aucun compte utilisateur !");
         }
 
         return $this->render('login/changePassword.html.twig');
     }
 
-    #[Route('/resetPassword/{login}/{token}', name: 'resetPassword', methods: ['GET', 'POST'])]
-    public function resetPassword() {
+    #[Route('/resetPassword/{token}', name: 'resetPassword', methods: ['GET', 'POST'])]
+    public function resetPassword(UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager) {
 
+        $request = Request::createFromGlobals();
+
+        $userLogin = $request->request->get("login");
+
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['login' => $userLogin]);
+        $firstPassword = $request->request->get("password");
+        $secondPassword = $request->request->get("secondPassword");
+
+
+        if ($user && $firstPassword === $secondPassword) {
+            $user->setToken(null);
+            $user->setPassword($hasher->hashPassword($user, $firstPassword));
+            $entityManager->flush();
+            $this->redirectToRoute('login');
+            $this->addFlash(type: "error", message: "Bravo !");
+        } else {
+            $this->addFlash(type: "error", message: "Ce login n'est associé à aucun compte utilisateur !");
+        }
+
+        return $this->render("login/resetPassword.html.twig");
     }
 }
