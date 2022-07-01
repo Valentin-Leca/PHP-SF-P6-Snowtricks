2c0259e754f9a98b32074db1a42e4f4dac70dc25
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2022-07-01 17:17:43 +0200

    Add avatar img choice in UserType (WIP)

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -67,6 +69,19 @@ class UserType extends AbstractType
                 "constraints" => [
                     new Email(['message' => "Veuillez saisir une adresse email valide. Ex : John.Doe@google.com"]),
                 ]
+            ])
+            ->add('avatar', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                ],
+                'attr' => [
+                    'class' => 'form-control rounded-0',
+                ],
+                'required' => false,
+                'expanded' => true,
+                'multiple' => false,
             ])
         ;
     }
