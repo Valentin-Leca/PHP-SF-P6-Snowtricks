d995ed6b16cea87e10d9ab80e6dd856c0e0a60a2
Valentin Lecavelier Des Etangs <valentin.val78@hotmail.fr>
2022-07-15 19:53:16 +0200

    Add media input / media.js file and MediaType.php

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Group;
+use App\Entity\Media;
 use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,13 +30,22 @@ class TrickType extends AbstractType
                     'placeholder' => 'Description de votre figure',
                 ],
             ])
+            ->add('media', CollectionType::class, [
+                'entry_type' => MediaType::class,
+                'attr' => [
+                    'class' =>'form-control rounded-0',
+                    'placeholder' => 'Ajoutez votre image',
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
             ->add('grouptrick', EntityType::class, [
                 'class' => Group::class,
                 'label_attr' => [
                     'class' => "label_trick_grouptrick"
                 ],
                 'label' => "Catégorie",
-            ])
+            ]);
         ;
     }
 
