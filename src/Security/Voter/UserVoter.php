3df7f073415c4be802c29956f40c3dd7ccbdbd01
Valentin LECAVELIER DES ETANGS <vlecavelierdesetangs@jems-group.com>
2022-10-05 12:22:50 +0200

    test correct "Your project should not use invalid parameter and return typehints" symfony insight

--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -12,13 +12,13 @@ class UserVoter extends Voter {
     public const EDIT = 'POST_EDIT';
     public const VIEW = 'POST_VIEW';
 
-    protected function supports(string $attribute, $subject): bool {
+    protected function supports(string $attribute,mixed $subject): bool {
 
         return in_array($attribute, [self::EDIT, self::VIEW])
             && $subject instanceof User;
     }
 
-    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool {
+    protected function voteOnAttribute(string $attribute,mixed $subject, TokenInterface $token): bool {
 
         $user = $token->getUser();
         // if the user is anonymous, do not grant access
